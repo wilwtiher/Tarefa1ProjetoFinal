fb7811fd13316530fc8e9e91553b0a39e2ca965b
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-23 11:48:32 -0300

    comecando a programacao no while

--- ProjetoFinal.c
+++ ProjetoFinal.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-#include "hardware/pwm.h"
 #include "pico/stdlib.h"
 #include "hardware/clocks.h"
 #include "inc/ssd1306.h"
@@ -23,11 +22,13 @@
 #define joybutton 22 // Botão A = 5, Botão B = 6 , BotãoJoy = 22
 #define VRY_PIN 26   // Pino do Joystick Y
 #define VRX_PIN 27   // Pino do Joystick X
+#define buzzer 10    // Pino do buzzer A
 
 // Variáveis globais
 static volatile uint32_t last_time = 0;     // Armazena o tempo do último evento (em microssegundos)
-static volatile uint32_t tempo_media = 0;   // Armazena o tempo do último evento (em microssegundos)
-static volatile uint32_t ponto_grafico = 0; // Armazena o tempo do último evento (em microssegundos)
+static volatile uint32_t tempo_media = 0;   // Armazena o tempo para a media (em microssegundos)
+static volatile uint32_t ponto_grafico = 0; // Armazena o tempo para o grafico (em microssegundos)
+static volatile uint32_t current_time = 0;  // Armazena o tempo atual (em microssegundos)
 bool grafico = true;                        // Se mostra ou nao o grafico
 bool pressaoxbatimento = true;              // Grafico da pressao ou do batimento
 bool Verde = false;                         // Variavel para Led Verde
@@ -44,54 +45,84 @@ int8_t cbatimentos = 0;                     // Aonde armazena o valor do batimen
 void gpio_irq_handler(uint gpio, uint32_t events)
 {
     // Obtém o tempo atual em microssegundos
-    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    current_time = to_us_since_boot(get_absolute_time());
     // Verifica se passou tempo suficiente desde o último evento
     if (current_time - last_time > 150000) // 150 ms de debouncing
     {
         if (gpio == joybutton)
         {
-            
         }
         if (gpio == botao_pinA)
         {
-            
         }
         if (gpio == botao_pinB)
         {
-            
         }
         last_time = current_time; // Atualiza o tempo do último evento
     }
-}
-
-// Funcao para iniciar pino PWM
-uint pwm_init_gpio(uint gpio, uint wrap)
-{
-    gpio_set_function(gpio, GPIO_FUNC_PWM);
-
-    uint slice_num = pwm_gpio_to_slice_num(gpio);
-    pwm_set_wrap(slice_num, wrap);
-
-    pwm_set_enabled(slice_num, true);
-    return slice_num;
 }
 
 int main()
 {
     stdio_init_all();
 
+    gpio_init(joybutton);
+    gpio_set_dir(joybutton, GPIO_IN); // Configura o pino como entrada
+    gpio_pull_up(joybutton);          // Habilita o pull-up interno
+    // Configuração da interrupção com callback
+    gpio_set_irq_enabled_with_callback(joybutton, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    gpio_init(botao_pinA);
+    gpio_set_dir(botao_pinA, GPIO_IN); // Configura o pino como entrada
+    gpio_pull_up(botao_pinA);          // Habilita o pull-up interno
+    // Configuração da interrupção com callback
+    gpio_set_irq_enabled_with_callback(botao_pinA, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    gpio_init(botao_pinB);
+    gpio_set_dir(botao_pinB, GPIO_IN); // Configura o pino como entrada
+    gpio_pull_up(botao_pinB);          // Habilita o pull-up interno
+    // Configuração da interrupção com callback
+    gpio_set_irq_enabled_with_callback(botao_pinB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    gpio_init(led_GREEN);              // Inicializa o pino do LED Verde
+    gpio_set_dir(led_GREEN, GPIO_OUT); // Configura o pino como saída
+    gpio_init(led_BLUE);               // Inicializa o pino do LED Azul
+    gpio_set_dir(led_BLUE, GPIO_OUT);  // Configura o pino como saída
+    gpio_init(led_RED);                // Inicializa o pino do LED Vermelho
+    gpio_set_dir(led_RED, GPIO_OUT);   // Configura o pino como saída
+
+    adc_init();
+    adc_gpio_init(VRY_PIN);
+    adc_gpio_init(VRX_PIN);
+
+    uint32_t last_print_time = 0;
+
+    // Para o display
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+    ssd1306_t ssd;                                                // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd);                                         // Configura o display
+    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+    // Limpa o display.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd); // Atualiza o display
 
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
-
+    for (int i = 0; i < 128; i++)
+    { // Zerar variaveis para evitar lixo armazenado
+        pressoes[i] = 0;
+        batimentos[i] = 0;
+    }
     while (true)
     {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        current_time = to_us_since_boot(get_absolute_time());
+        adc_select_input(1);
+        int16_t vrx_value = adc_read();
+        adc_select_input(0);
+        int16_t vry_value = adc_read();
     }
 }
