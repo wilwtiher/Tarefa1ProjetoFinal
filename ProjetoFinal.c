7a42671a51ebfc0dc2f3ab2a50e2c3a7b2d20f18
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-24 10:35:13 -0300

    plotagem grafico simples

--- ProjetoFinal.c
+++ ProjetoFinal.c
@@ -25,25 +25,25 @@
 #define buzzer 10    // Pino do buzzer A
 
 // Variáveis globais
-static volatile uint32_t last_time = 0;     // Armazena o tempo do último evento (em microssegundos)
-static volatile uint32_t tempo_media = 0;   // Armazena o tempo para a media (em microssegundos)
-static volatile uint32_t current_time = 0;  // Armazena o tempo atual (em microssegundos)
+static volatile uint32_t last_time = 0;    // Armazena o tempo do último evento (em microssegundos)
+static volatile uint32_t tempo_media = 0;  // Armazena o tempo para a media (em microssegundos)
+static volatile uint32_t current_time = 0; // Armazena o tempo atual (em microssegundos)
 bool alarme;
-bool grafico = false;                        // Se mostra ou nao o grafico
-bool pressaoxbatimento = true;              // Grafico da pressao ou do batimento
-bool Verde = false;                         // Variavel para Led Verde
-bool Amarelo = false;                       // Variavel para Led Amarelo
-bool Vermelho = false;                      // Variavel para Led Vermelho
+bool grafico = false;          // Se mostra ou nao o grafico
+bool pressaoxbatimento = true; // Grafico da pressao ou do batimento
+bool Verde = false;            // Variavel para Led Verde
+bool Amarelo = false;          // Variavel para Led Amarelo
+bool Vermelho = false;         // Variavel para Led Vermelho
 int16_t contapressao = 0;
 int16_t contabatimentos = 0;
 int8_t apressao;
 int8_t abatimentos;
-int8_t pressoes[128];                       // Pontos grafico da pressao
-int8_t batimentos[128];                     // Pontos grafico do batimento
-int8_t mpressao[10];                        // Media da pressao (10 pontos por 1 segundo)
-int8_t mbatimentos[10];                     // Media do batimento (10 pontos por 1 segundo)
-int8_t cgrafico = 0;                        // Aonde armazena o valor da pressao e batimento atual
-int8_t cmedia = 0;                     // Aonde armazena o valor da media
+int8_t pressoes[128];   // Pontos grafico da pressao
+int8_t batimentos[128]; // Pontos grafico do batimento
+int8_t mpressao[10];    // Media da pressao (10 pontos por 1 segundo)
+int8_t mbatimentos[10]; // Media do batimento (10 pontos por 1 segundo)
+int8_t cgrafico = 0;    // Aonde armazena o valor da pressao e batimento atual
+int8_t cmedia = 0;      // Aonde armazena o valor da media
 
 // Função de interrupção com debouncing
 void gpio_irq_handler(uint gpio, uint32_t events)
@@ -58,17 +58,21 @@ void gpio_irq_handler(uint gpio, uint32_t events)
         }
         if (gpio == botao_pinA)
         {
-            if(grafico){
+            if (grafico)
+            {
                 pressaoxbatimento = !pressaoxbatimento;
-            }else{
+            }
+            else
+            {
                 alarme = !alarme;
                 if (alarme)
                 {
                     gpio_put(buzzer, true);
-                }else{
+                }
+                else
+                {
                     gpio_put(buzzer, false);
                 }
-                
             }
         }
         if (gpio == botao_pinB)
@@ -143,25 +147,25 @@ int main()
         adc_select_input(0);
         int16_t vry_value = adc_read();
 
-
         apressao = vrx_value / 17;
         abatimentos = vry_value / 25;
 
-
-        if (current_time - tempo_media > 100000){
+        if (current_time - tempo_media > 100000)
+        {
             tempo_media = to_us_since_boot(get_absolute_time());
             mpressao[cmedia] = apressao;
             mbatimentos[cmedia] = abatimentos;
             cmedia++;
-            if(cmedia == 10){
+            if (cmedia == 10)
+            {
                 cmedia = 0;
                 for (int i = 0; i < 10; i++)
                 {
                     contapressao = contapressao + mpressao[i];
                     contabatimentos = contabatimentos + mbatimentos[i];
                 }
-                pressoes[cgrafico] = contapressao/10;
-                batimentos[cgrafico] = contabatimentos/10;
+                pressoes[cgrafico] = contapressao / 10;
+                batimentos[cgrafico] = contabatimentos / 10;
                 contapressao = 0;
                 contabatimentos = 0;
                 cgrafico++;
@@ -171,15 +175,35 @@ int main()
                 }
             }
         }
-        if(grafico){
-            if(pressaoxbatimento){ // pressao
-
-            }else{// batimento
-
+        if (grafico)
+        { // mostrar graficos
+            if (pressaoxbatimento)
+            { // pressao
+                for (int i = 0; i < 128; i++)
+                {
+                    contapressao = pressoes[i] / 2;
+                    ssd1306_fill(&ssd, false);
+                    ssd1306_draw_string(&ssd, "Pressao" , 100, 120);
+                    ssd1306_vline(&ssd, i, 0, contapressao, 1);
+                    contapressao = 0;
+                }
             }
-        }else{
-
+            else
+            { // batimento
+                for (int i = 0; i < 128; i++)
+                {
+                    contabatimentos = pressoes[i] / 1.64;
+                    ssd1306_fill(&ssd, false);
+                    ssd1306_draw_string(&ssd, "Batimentos" , 88, 120);
+                    ssd1306_vline(&ssd, i, 0, contabatimentos, 1);
+                    contabatimentos = 0;
+                }
+            }
         }
-
+        else
+        { // mostrar valores atuais
+            ssd1306_fill(&ssd, false);
+            ssd1306_rect(&ssd, 3, 3, 122, 58, 1, 0); // Desenha um retângulo
+        }
     }
 }
